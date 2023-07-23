e57c1d366a4636d9a7d6471f1966e5f30d1d113b
elareh <66442150+elareh@users.noreply.github.com>
2023-07-23 15:14:39 -0300

    correcciones

--- filtro_id.js
+++ filtro_id.js
@@ -1,5 +1,8 @@
 var page = "https://rickandmortyapi.com/api/character/?page=";
 var CharactersList = [];
+let currentPage = 1;
+const charactersPerPage = 20;
+
 
 function CreateObject (id, name, species, gender, image){
   const newCharacter = {
@@ -9,7 +12,6 @@ function CreateObject (id, name, species, gender, image){
     genero: gender,
     image: image
   }; 
-  console.log("Se crea un Object con este Id: " + id)
   CharactersList.push(newCharacter);
 }
 
@@ -18,25 +20,25 @@ function loopThroughApi(page) {
   fetch(page)
     .then(response => response.json())
     .then(data => {
-      data.results.forEach(result => {
-        var idResult = result.id;
-        var nameResult = result.name;
-        var speciesResult = result.species;
-        var genderResult = result.gender;
-        var imageResult = result.image;
-        CreateObject(idResult, nameResult, speciesResult, genderResult, imageResult);
-        
+      // Obtener el rango de personajes para la página actual
+      const startIndex = (currentPage - 1) * charactersPerPage;
+      const endIndex = currentPage * charactersPerPage;
+
+      data.results.forEach((result, index) => {
+        if (index >= startIndex && index < endIndex) {
+          var idResult = result.id;
+          var nameResult = result.name;
+          var speciesResult = result.species;
+          var genderResult = result.gender;
+          var imageResult = result.image;
+          CreateObject(idResult, nameResult, speciesResult, genderResult, imageResult);
+        }
       });
 
-      // Verificar si hay más páginas disponibles (si next no es null)
       if (data.info.next) {
-        // Actualizar el parámetro page con la siguiente URL
         page = data.info.next;
-        // Hacer una llamada recursiva para obtener los datos de la siguiente página
         loopThroughApi(page);
       } else {
-        // Cuando no hay más páginas, puedes imprimir los resultados o hacer lo que necesites aquí
-        console.log(CharactersList);
         mostrarElementos();
       }
 
@@ -48,36 +50,42 @@ function loopThroughApi(page) {
 
 function mostrarElementos() {
   var container = document.getElementById('charactersFromApi');
-
-  // Limpiamos el contenido existente en el contenedor
   container.innerHTML = '';
 
-  // Mostrar solo los primeros 20 personajes (IDs del 0 al 19)
-  var numPersonajesAMostrar = 20;
-  for (var i = 0; i < numPersonajesAMostrar; i++) {
+  var startIndex = (currentPage - 1) * charactersPerPage;
+  var endIndex = currentPage * charactersPerPage;
+
+  for (var i = startIndex; i < endIndex && i < CharactersList.length; i++) {
     var personaje = CharactersList[i];
+
 
     // Crear el div que contendrá la información del personaje
     var characterDiv = document.createElement('div');
     characterDiv.classList.add('character');
+    characterDiv.classList.add('character-card');
+    characterDiv.classList.add('grid-container');
 
     // Crear la imagen del personaje
     var characterImage = document.createElement('img');
     characterImage.src = personaje.image;
     characterImage.alt = personaje.nombre;
+    characterImage.classList.add("imagenCharacter");
     characterDiv.appendChild(characterImage);
 
     // Crear el párrafo con el nombre del personaje
     var characterName = document.createElement('p');
     characterName.textContent = 'Nombre: ' + personaje.nombre;
+    characterName.classList.add('character-name');
     characterDiv.appendChild(characterName);
 
     // Crear el párrafo con la especie del personaje
     var characterSpecies = document.createElement('p');
+    characterSpecies.classList.add('character-info');
     characterSpecies.textContent = 'Especie: ' + personaje.especie;
     characterDiv.appendChild(characterSpecies);
 
     var characterGender = document.createElement('p');
+    characterGender.classList.add('character-info');
     characterGender.textContent = 'Género: ' + personaje.genero;
     characterDiv.appendChild(characterGender);
 
@@ -85,21 +93,33 @@ function mostrarElementos() {
     container.appendChild(characterDiv);
   }
 }
+console.log(currentPage);
 
-// function mostrarElementos() {
-//   var container = document.getElementById('charactersFromApi');
-//   var html = '';
 
-//   // Iterar a través de los elementos del objeto JSON
-//   for (var i = 0; i < CharactersList.length; i++) {
-//       var elemento = CharactersList[i];
-//       html += '<p>Nombre: ' + elemento.nombre;
-//       // Modifica la estructura según los datos que tenga tu objeto JSON
-//   }
 
-//   // Mostrar los elementos en el contenedor
-//   container.innerHTML = html;
-// }
+document.getElementById('previousPage').addEventListener('click', function() {
+  if (currentPage > 1) {
+    currentPage--;
+    console.log(currentPage);
+    mostrarElementos();
+  }
+});
+
+
+document.getElementById('nextPage').addEventListener('click', function() {
+  if (currentPage < Math.ceil(CharactersList.length / charactersPerPage)) {
+    currentPage++;
+    console.log(currentPage);
+    mostrarElementos();
+  }
+
+  
+});
+
+
+
+
+
 
 loopThroughApi(page);
 console.log(CharactersList);
