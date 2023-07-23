0ab15149e83f2421003890754ffe8b5248e62292
elareh <66442150+elareh@users.noreply.github.com>
2023-07-23 16:28:25 -0300

    Searchbar

--- filtro_id.js
+++ filtro_id.js
@@ -3,24 +3,21 @@ var CharactersList = [];
 let currentPage = 1;
 const charactersPerPage = 20;
 
-
-function CreateObject (id, name, species, gender, image){
+function CreateObject(id, name, species, gender, image) {
   const newCharacter = {
     id: id,
     nombre: name,
     especie: species,
     genero: gender,
     image: image
-  }; 
+  };
   CharactersList.push(newCharacter);
 }
-
 
 function loopThroughApi(page) {
   fetch(page)
     .then(response => response.json())
     .then(data => {
-      // Obtener el rango de personajes para la página actual
       const startIndex = (currentPage - 1) * charactersPerPage;
       const endIndex = currentPage * charactersPerPage;
 
@@ -42,6 +39,10 @@ function loopThroughApi(page) {
         mostrarElementos();
       }
 
+      if (document.getElementById('buscador').value.trim() !== '') {
+        buscarElementos();
+        return;
+      }
     })
     .catch(error => {
       console.error('Error:', error);
@@ -58,27 +59,21 @@ function mostrarElementos() {
   for (var i = startIndex; i < endIndex && i < CharactersList.length; i++) {
     var personaje = CharactersList[i];
 
-
-    // Crear el div que contendrá la información del personaje
     var characterDiv = document.createElement('div');
     characterDiv.classList.add('character');
     characterDiv.classList.add('character-card');
-    characterDiv.classList.add('grid-container');
 
-    // Crear la imagen del personaje
     var characterImage = document.createElement('img');
     characterImage.src = personaje.image;
     characterImage.alt = personaje.nombre;
     characterImage.classList.add("imagenCharacter");
     characterDiv.appendChild(characterImage);
 
-    // Crear el párrafo con el nombre del personaje
     var characterName = document.createElement('p');
     characterName.textContent = 'Nombre: ' + personaje.nombre;
     characterName.classList.add('character-name');
     characterDiv.appendChild(characterName);
 
-    // Crear el párrafo con la especie del personaje
     var characterSpecies = document.createElement('p');
     characterSpecies.classList.add('character-info');
     characterSpecies.textContent = 'Especie: ' + personaje.especie;
@@ -89,32 +84,49 @@ function mostrarElementos() {
     characterGender.textContent = 'Género: ' + personaje.genero;
     characterDiv.appendChild(characterGender);
 
-    // Agregar el div del personaje al contenedor
     container.appendChild(characterDiv);
   }
 }
-console.log(currentPage);
-
-
 
 document.getElementById('previousPage').addEventListener('click', function() {
   if (currentPage > 1) {
     currentPage--;
-    console.log(currentPage);
     mostrarElementos();
   }
 });
-
 
 document.getElementById('nextPage').addEventListener('click', function() {
   if (currentPage < Math.ceil(CharactersList.length / charactersPerPage)) {
     currentPage++;
-    console.log(currentPage);
     mostrarElementos();
   }
-
-  
 });
+
+
+document.getElementById('buscador').addEventListener('input', function() {
+  buscarElementos();
+});
+
+var originalCharactersList = CharactersList;
+
+function buscarElementos() {
+  var searchTerm = document.getElementById('buscador').value.toLowerCase();
+
+  if (searchTerm === '') {
+    currentPage = 1;
+    CharactersList = originalCharactersList; // Restore original values
+    mostrarElementos();
+    return;
+  }
+
+  var resultados = originalCharactersList.filter(function(personaje) {
+    return personaje.nombre.toLowerCase().includes(searchTerm) || personaje.especie.toLowerCase().includes(searchTerm) || personaje.genero.toLowerCase().includes(searchTerm);
+  });
+
+  currentPage = 1;
+  CharactersList = resultados;
+  mostrarElementos();
+}
 
 
 
