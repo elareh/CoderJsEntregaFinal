49427ffcdb1d8e3e9c3a755d9ae205a9a8021ee4
elareh <66442150+elareh@users.noreply.github.com>
2023-07-23 22:05:49 -0300

    formContacto

--- js/app.js
+++ js/app.js
@@ -134,27 +134,6 @@ function buscarElementos() {
   mostrarElementos();
 }
 
-$(document).ready(function() {
-  $('.nav-link').on('click', function(e) {
-    e.preventDefault();
-    var url = $(this).attr('href');
-    
-    if (url === 'https://elareh.github.io/contacto.html') {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Page not found',
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'OK',
-      });
-    } else {
-      window.location.href = url;
-    }
-  });
-});
-
-
-
 
 
 loopThroughApi(page);
